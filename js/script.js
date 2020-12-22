aa6bce0eecf7b8aa472cf1a9215433b1eb853ded
alessandro <finialessandro90@gmail.com>
2020-12-22 16:18:00 +0100

    done

--- js/script.js
+++ js/script.js
@@ -20,19 +20,31 @@ console.log(numbers);
 
 /* In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100. */
 var inputNumbers = [];
-var maxInput = 100 - numbers.length;
+var maxInput = 20 - numbers.length;
+var stopCicle = false;
 
-for (var i = 0; i < maxInput; i++){
-    var userNumbers = parseInt(prompt('Inserisci un numero\ncompreso tra 1 e 100'));
-    
+for (var i = 0; i < maxInput && stopCicle == false; i++){
+    var userNumbers = parseInt(prompt('Inserisci un numero\ncompreso tra 1 e 100'));    
     /* L’utente non può inserire più volte lo stesso numero. */
     if (!inputNumbers.includes(userNumbers)){
         inputNumbers.push(userNumbers);        
     } else {
-        alert('Non puoi iserire due volte\nlo stesso numero!');
+        alert('Non puoi iserire più volte\nlo stesso numero!');
+        maxInput += 1;
     }
+    /* Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero. */
+    if (numbers.includes(userNumbers)){
+        alert('Hai perso!\n' + 'Hai inserito ' + (inputNumbers.length - 1) + ' numeri consentiti');
+        stopCicle = true;
+    } 
 }
 console.log(inputNumbers);
+
+/* Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito. */
+if (!numbers.includes(inputNumbers) && inputNumbers.length == maxInput && stopCicle == false){
+    alert('Hai vinto!\n' + 'Hai inserito ' + inputNumbers.length + ' numeri consentiti');
+}
+
 
 
 
